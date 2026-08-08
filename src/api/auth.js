a27917e0f433f80e315b28388daadf895f43d0cb
IsmaelG <ismaelgn89@gmail.com>
2026-08-08 20:07:41 +0200

    feat(auth): integrate register with api

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,3 +4,11 @@ export const login = (email, password) =>
   api.post("/login", { email, password });
 
 export const logout = () => api.post("/logout");
+
+export const register = (name, email, password, passwordConfirmation) =>
+  api.post("/register", {
+    name,
+    email,
+    password,
+    password_confirmation: passwordConfirmation,
+  });
